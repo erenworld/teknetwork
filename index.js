1bed35dacc28d3f6cf7aa969f6e11a0a6c72a466
eren <erenxturkoglu@gmail.com>
2025-03-19 11:02:33 +0100

    add new members

--- index.js
+++ index.js
@@ -18,9 +18,15 @@ document.querySelector('#app').innerHTML = `
       <section id="students" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-16 w-full max-w-4xl mx-auto"> </section>
 
     </div>
-    <button class="btnSubmit flex items-center gap-1 left-1/2 -translate-x-1/2 z-50  bottom-8 fixed z-0 h-12 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500 cursor-pointer"><a href="https://github.com/erenworld/teknetwork" target="_blank">Add your website via Github</a><span><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span></button>
+    <button class="btnSubmit flex items-center gap-1 left-1/2 -translate-x-1/2 z-50  bottom-8 fixed z-0 h-12 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500 cursor-pointer">Add your profil<span><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span></button>
   </div>
 `
+
+const btnSubmit = document.querySelector(".btnSubmit");
+
+btnSubmit.addEventListener("click", () => {
+  alert("hi");
+})
 
 async function loadStudents() {
   const response = await fetch("/students.json");
